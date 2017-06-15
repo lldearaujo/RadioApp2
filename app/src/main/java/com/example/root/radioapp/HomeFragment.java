707e89a36b7edf6f310186ba1af4329ff8cd460f
Leonardo Lins <lldearaujo@hotmail.com>
2017-06-15 17:19:52 -0300

    Commit 005 - 15/06/2017 Inclusão do Facebook Button Login Criação da classe e view SingUp

---
+++ app/src/main/java/com/example/root/radioapp/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.root.radioapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Intent;
+
+
+public class HomeFragment extends Fragment{
+
+    public static HomeFragment newInstance() {
+
+        return new HomeFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/root/radioapp/SingUp.java
@@ -0,0 +1,17 @@
+package com.example.root.radioapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 15/06/17.
+ */
+
+public class SingUp extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
