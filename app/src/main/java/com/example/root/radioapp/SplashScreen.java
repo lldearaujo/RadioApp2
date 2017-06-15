cdf8209953f56e0a4271c8088ebbf20371f2aa1d
Leonardo Lins <lldearaujo@hotmail.com>
2017-06-15 09:52:08 -0300

    Commit 004 - 15/06/2017 Inclusão da classe e view Login.

--- app/src/main/java/com/example/root/radioapp/SplashScreen.java
+++ app/src/main/java/com/example/root/radioapp/SplashScreen.java
@@ -21,7 +21,7 @@ public class SplashScreen extends AppCompatActivity {
     }
 
     private void mostrarLogin(){
-        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        Intent intent = new Intent(SplashScreen.this, Login.class);
         startActivity(intent);
         finish();
     }
